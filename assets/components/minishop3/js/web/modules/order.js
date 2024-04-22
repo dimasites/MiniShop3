fe1c0210f0e01c371a211f783b31832e4a7506e0
biz87 <info@megawebs.kz>
2024-04-22 17:21:56 +0500

    решение js конфликтоов

--- assets/components/minishop3/js/web/modules/order.js
+++ assets/components/minishop3/js/web/modules/order.js
@@ -1,50 +1,42 @@
 ms3.order = {
-    init () {
-        const orderForms = document.querySelectorAll('.ms3_order_form')
-        if (orderForms.length > 0) {
-            orderForms.forEach(orderForm => {
-                ms3.order.formListener(orderForm)
-            })
-        }
-    },
-    formListener (customerForm) {
-        const inputs = customerForm.querySelectorAll('input, textarea, select')
-        if (inputs.length > 0) {
-            inputs.forEach(input => {
-                ms3.order.changeInputListener(input)
-            })
-        }
-    },
-    changeInputListener (input) {
-        input.addEventListener('change', async () => {
-            const form = input.closest('.ms3_order_form')
-            const parent = input.closest('div')
-            parent.classList.remove('was-validated')
-            input.classList.remove('is-invalid')
-            parent.querySelector('.invalid-feedback').textContent = ''
-            const formData = new FormData()
-            formData.append('key', input.name)
-            formData.append('value', input.value)
-            const response = await ms3.order.add(formData)
-            if (response.success === true) {
-                parent.classList.add('was-validated')
-                //TODO не менять radio, checkbox, select
-                input.value = response.data[input.name]
-            } else {
-                parent.classList.add('was-validated')
-                input.classList.add('is-invalid')
-                parent.querySelector('.invalid-feedback').textContent = response.message
-            }
-        })
-    },
-    async add (formData) {
-        formData.append('ms3_action', 'order/add')
-        const response = await ms3.request.send(formData)
-        //TODO callback, event
-        return response
-    },
-    async remove (formData) {
-        formData.append('ms3_action', 'order/remove')
-        const response = await ms3.request.send(formData)
-    },
+  init () {
+    const orderForms = document.querySelectorAll('.ms3_order_form')
+    orderForms.forEach(orderForm => ms3.order.formListener(orderForm))
+  },
+  formListener (customerForm) {
+    const inputs = customerForm.querySelectorAll('input, textarea, select')
+    inputs.forEach(input => ms3.order.changeInputListener(input))
+  },
+  changeInputListener (input) {
+    input.addEventListener('change', async () => {
+      // const form = input.closest('.ms3_order_form')
+      const parent = input.closest('div')
+      parent.classList.remove('was-validated')
+      input.classList.remove('is-invalid')
+      parent.querySelector('.invalid-feedback').textContent = ''
+      const formData = new FormData()
+      formData.append('key', input.name)
+      formData.append('value', input.value)
+      const response = await ms3.order.add(formData)
+      if (response.success === true) {
+        parent.classList.add('was-validated')
+        // TODO не менять radio, checkbox, select
+        input.value = response.data[input.name]
+      } else {
+        parent.classList.add('was-validated')
+        input.classList.add('is-invalid')
+        parent.querySelector('.invalid-feedback').textContent = response.message
+      }
+    })
+  },
+  async add (formData) {
+    formData.append('ms3_action', 'order/add')
+    const response = await ms3.request.send(formData)
+    // TODO callback, event
+    return response
+  },
+  async remove (formData) {
+    formData.append('ms3_action', 'order/remove')
+    await ms3.request.send(formData)
+  }
 }
